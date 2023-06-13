ef1ceac173d97f53ffa03605594a251f480924d7
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2023-06-13 10:27:31 -0300

    bug fix categories

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -8,7 +8,7 @@ const EventsPage = ({data}) => {
       <h1>Event Page</h1>
       <div>
         {data.map(ev => (
-          <a key={ev.id} href={`/events/${ev.id}`}><Image src={ev.image} alt={ev.title} width={300} height={100} /><h2>{ev.title}</h2>
+          <a key={ev.id} href={`/events/${ev.id}`}><Image src={ev.image} alt={ev.title} width={300} height={300} /><h2>{ev.title}</h2>
           
             </a>
         ))}
